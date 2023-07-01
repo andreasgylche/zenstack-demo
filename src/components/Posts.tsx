b7aaec51d960ef01260e9a5f769411e7e86b120a
Andreas Vifert <andreasgylche@gmail.com>
2023-07-01 17:27:16 +0200

    changed some shit

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -30,33 +30,41 @@ const Posts = ({ user }: { user: AuthUser }) => {
   }
 
   return (
-    <div className="container flex flex-col">
+    <div className="flex w-full flex-col">
       <button
-        className="rounded border border-black p-2 text-lg"
+        className="rounded border border-zinc-600 p-2 text-lg"
         onClick={() => void onCreatePost()}
       >
         + Create Post
       </button>
 
-      <ul className="container mt-8 flex flex-col gap-2">
+      <div className="mt-8 flex w-full flex-col gap-4">
         {posts?.map((post) => (
-          <li key={post.id} className="flex items-end justify-between gap-4">
-            <div className="mb-4 w-full max-w-md rounded-lg bg-white px-6 py-8 shadow-md">
-              <h2 className="mb-4 text-2xl font-semibold">{post.title}</h2>
-              <p className="mb-2 text-gray-600">Author: {post.author.email}</p>
-              <p className="mb-6 text-gray-600">
+          <div key={post.id} className="flex items-end justify-between gap-4">
+            <div className="w-full rounded-lg bg-zinc-800 px-6 py-8 shadow-md">
+              <h2 className="mb-4 text-2xl font-bold">{post.title}</h2>
+              <p className="mb-2 text-zinc-400">Author: {post.author.email}</p>
+              <p className="mb-6 text-zinc-400">
                 {post.published ? "Published" : "Not published"}
               </p>
-              <button
-                className="w-full rounded-md bg-red-500 py-2 font-bold text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
-                onClick={() => void onDelete(post)}
-              >
-                Delete
-              </button>
+              <div className="flex gap-2">
+                <button
+                  className="w-full rounded-md bg-zinc-900 py-2 font-bold text-zinc-100 transition-colors hover:bg-zinc-950 focus:outline-none focus:ring-2 focus:ring-zinc-600 focus:ring-opacity-50"
+                  onClick={() => void onTogglePublished(post)}
+                >
+                  Publish
+                </button>
+                <button
+                  className="w-full rounded-md bg-red-500 py-2 font-bold text-zinc-100 transition-colors hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+                  onClick={() => void onDelete(post)}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,17 +1,9 @@
-import { signOut } from "next-auth/react";
 import { AuthUser } from "~/lib/types/AuthUser";
 
 const Welcome = ({ user }: { user: AuthUser }) => {
-  async function onSignout() {
-    await signOut({ redirect: false, callbackUrl: "/signin" });
-  }
-
   return (
     <div className="flex gap-4">
       <h3 className="text-lg">Welcome back, {user.email}</h3>
-      <button className="text-black underline" onClick={() => void onSignout()}>
-        Signout
-      </button>
     </div>
   );
 };
