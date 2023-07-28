e095e6a89a7cb6903457bf084a916d2d8b9105f2
Andreas Vifert <andreasgylche@gmail.com>
2023-07-28 09:03:54 +0200

    test

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,5 +1,15 @@
 import { useFindManyPost, useMutatePost } from "~/lib/hooks";
 import { AuthUser } from "~/lib/types/AuthUser";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/Avatar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/Card";
+import { Button } from "~/components/ui/Button";
 
 const Posts = ({ user }: { user: AuthUser }) => {
   const { createPost, updatePost, deletePost } = useMutatePost();
@@ -31,38 +41,45 @@ const Posts = ({ user }: { user: AuthUser }) => {
 
   return (
     <div className="flex w-full flex-col">
-      <button
-        className="rounded border border-zinc-600 p-2 text-lg"
-        onClick={() => void onCreatePost()}
-      >
-        + Create Post
-      </button>
+      <Button onClick={() => void onCreatePost()}>+ Create Post</Button>
 
       <div className="mt-8 grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
         {posts?.map((post) => (
-          <div key={post.id} className="flex items-end justify-between gap-4">
-            <div className="w-full rounded-lg bg-zinc-800 px-6 py-8 shadow-md">
-              <h2 className="mb-4 text-2xl font-bold">{post.title}</h2>
-              <p className="mb-2 text-zinc-400">Author: {post.author.email}</p>
-              <p className="mb-6 text-zinc-400">
+          <Card key={post.id}>
+            <CardHeader>
+              <CardTitle className="mb-2 font-bold">{post.title}</CardTitle>
+              <CardDescription className="flex items-center gap-2">
+                <Avatar className="h-8 w-8">
+                  <AvatarImage src={post.author.image!} />
+                  <AvatarFallback>
+                    {post.author.email?.slice(0, 2).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                {post.author.email}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>This is the card content.</p>
+              <p className="darK:text-zinc-400 mb-6">
                 {post.published ? "Published" : "Not published"}
               </p>
-              <div className="flex gap-2">
-                <button
-                  className="w-full rounded-md bg-zinc-900 py-2 font-bold text-zinc-100 transition-colors hover:bg-zinc-950 focus:outline-none focus:ring-2 focus:ring-zinc-600 focus:ring-opacity-50"
-                  onClick={() => void onTogglePublished(post)}
-                >
-                  Publish
-                </button>
-                <button
-                  className="w-full rounded-md bg-red-500 py-2 font-bold text-zinc-100 transition-colors hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
-                  onClick={() => void onDelete(post)}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
+            </CardContent>
+            <CardFooter>
+              {post.authorId === user.id && (
+                <div className="flex gap-2">
+                  <Button
+                    variant="default"
+                    onClick={() => void onTogglePublished(post)}
+                  >
+                    Publish
+                  </Button>
+                  <Button variant="ghost" onClick={() => void onDelete(post)}>
+                    Delete
+                  </Button>
+                </div>
+              )}
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
